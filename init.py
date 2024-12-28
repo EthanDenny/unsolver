a082b01bc41e04f5242c5efc5c93a8d79d9585a0
Ethan Denny <ethandenny@protonmail.com>
2024-12-28 15:24:24 -0330

    Add and sub

--- init.py
+++ init.py
@@ -1,62 +1,88 @@
 import random
 
-TECHNIQUES = {
-    "addition": True,
-    "subtraction": True,
-    "multiplication": True,
-    "division": False,
-}
 
-DEPTH = 3
+ops = ["+", "-"]
 
 
 class Node:
     def __init__(self, depth=1):
+        self.op = None
+        self.value = None
         self.left = None
         self.right = None
         self.depth = depth
 
 
 while True:
-    x = input("Please enter a number: ")
+    ANSWER = input("Please enter an answer: ")
 
     try:
-        x = int(x)
+        ANSWER = int(ANSWER)
         break
     except:
         print("That's not a number!")
 
+while True:
+    DEPTH = input("Please enter a max depth: ")
+
+    try:
+        DEPTH = int(DEPTH)
+        break
+    except:
+        print("That's not a number!")
+
+
 ROOT = Node()
-DEPTH = x
-NEEDS_CHILD = [ROOT]
 
 
 def expand_node(node):
     if node.depth == DEPTH:
         return
 
-    if random.randint(0, node.depth) != 0:
-        node.left = Node(node.depth + 1)
+    node.op = random.choice(ops)
+
+    node.left = Node(node.depth + 1)
+    if random.randint(1, 10) >= node.depth:
         expand_node(node.left)
 
-    if random.randint(0, node.depth) != 0:
-        node.right = Node(node.depth + 1)
+    node.right = Node(node.depth + 1)
+    if random.randint(1, 10) >= node.depth:
         expand_node(node.right)
 
 
-def print_node(node, depth=1):
-    for _ in range(depth - 1):
-        print("  ", end="")
+def evaluate_node(node, value):
+    if node.op == "+":
+        if value > 1:
+            left = random.randint(1, value - 1)
+            right = value - left
 
-    if node == None:
-        print("C")
+            evaluate_node(node.left, left)
+            evaluate_node(node.right, right)
+        else:
+            node.op = None
+            node.value = 1
+    elif node.op == "-":
+        left = random.randint(value + 1, value * 2)
+        right = left - value
+
+        evaluate_node(node.left, left)
+        evaluate_node(node.right, right)
     else:
-        print(depth)
-        print_node(node.left, depth + 1)
-        print_node(node.right, depth + 1)
+        node.value = value
+
+
+def print_node(node, depth=1):
+    if node.op == None:
+        s = node.value
+    else:
+        s = f"({print_node(node.left, depth + 1)} {node.op} {print_node(node.right, depth + 1)})"
+
+    if depth == 1:
+        print(s)
+    else:
+        return s
 
 
 expand_node(ROOT)
+evaluate_node(ROOT, ANSWER)
 print_node(ROOT)
-
-print(f"Your number is {x}")
