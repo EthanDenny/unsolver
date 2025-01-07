3f7f37b96001f5c8b08bd0837fa1f33a2aff76ed
Ethan Denny <ethandenny@protonmail.com>
2025-01-07 13:10:17 -0330

    Beeg update

--- unsolver-lib/src/lib.rs
+++ unsolver-lib/src/lib.rs
@@ -1,5 +1,4 @@
 use rand::seq::SliceRandom;
-use std::fmt::{Display, Error};
 use wasm_bindgen::prelude::*;
 
 #[derive(Clone, Copy)]
@@ -17,9 +16,10 @@ impl EngineFlags {
     }
 }
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 enum MathOp {
-    Const(Option<i32>), // If a constant is None, it has not yet been assigned a value
+    Null,
+    Const,
     Add,
     Sub,
     Mul,
@@ -30,35 +30,36 @@ impl MathOp {
     // Higher value => lower precedence
     fn precedence(&self) -> i32 {
         match self {
-            MathOp::Const(_) | MathOp::Div => 0,
+            MathOp::Const | MathOp::Div => 0,
             MathOp::Mul => 1,
             MathOp::Add | MathOp::Sub => 2,
+            MathOp::Null => panic!("{self:?} has no precedence"),
         }
     }
-}
 
-impl Display for MathOp {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn symbol(&self) -> Result<String, String> {
         match self {
-            MathOp::Const(Some(value)) => f.write_str(value.to_string().as_str()),
-            MathOp::Add => f.write_str("+"),
-            MathOp::Sub => f.write_str("-"),
-            MathOp::Mul => f.write_str("\\times"),
-            _ => Err(Error),
+            MathOp::Add => Ok("+".to_string()),
+            MathOp::Sub => Ok("-".to_string()),
+            MathOp::Mul => Ok("\\times".to_string()),
+            _ => Err(format!("The op {:?} has no associated symbol", self)),
         }
     }
 }
 
+#[derive(Debug)]
 struct MathNode {
     op: MathOp,
+    value: i32,
     children: Vec<MathNode>,
     depth: i32,
 }
 
 impl MathNode {
-    fn new(depth: i32) -> MathNode {
+    fn new(op: MathOp, value: i32, depth: i32) -> MathNode {
         MathNode {
-            op: MathOp::Const(None),
+            op,
+            value,
             children: Vec::new(),
             depth,
         }
@@ -81,76 +82,72 @@ fn expand_node(node: &mut MathNode, max_depth: i32, ops: &[MathOp], mut settings
         })
         .collect();
 
-    node.op = *filtered_ops
-        .choose(&mut rand::thread_rng())
-        .unwrap_or(&MathOp::Const(None));
+    if filtered_ops.len() > 0 {
+        node.op = *filtered_ops.choose(&mut rand::thread_rng()).unwrap();
+    } else {
+        node.op = MathOp::Const;
+        return;
+    }
 
     settings.division_parent = node.op == MathOp::Div;
 
-    let child_count = match node.op {
-        MathOp::Add | MathOp::Sub | MathOp::Mul | MathOp::Div => 2,
-        _ => 0,
-    };
-
-    for _ in 0..child_count {
-        let mut child = MathNode::new(node.depth + 1);
+    let mut create_child = |value: i32| {
+        let mut child = MathNode::new(MathOp::Null, value, node.depth + 1);
 
         if rand::random::<bool>() {
             expand_node(&mut child, max_depth, &ops, settings);
         }
 
-        node.children.push(child);
-    }
-}
+        if child.op == MathOp::Null {
+            child.op = MathOp::Const;
+        }
 
-fn evaluate_node(node: &mut MathNode, value: i32) -> Result<(), String> {
+        node.children.push(child);
+    };
+
     match node.op {
         MathOp::Add => {
-            if value > 1 {
-                let left = 1 + rand::random::<i32>().abs() % (value - 1);
-                let right = value - left;
+            if node.value > 1 {
+                let left = 1 + rand::random::<i32>().abs() % (node.value - 1);
+                let right = node.value - left;
 
-                evaluate_node(&mut node.children[0], left)?;
-                evaluate_node(&mut node.children[1], right)?;
+                create_child(left);
+                create_child(right);
             } else {
-                node.op = MathOp::Const(Some(1));
+                node.op = MathOp::Const;
+                node.value = 1;
             }
         }
         MathOp::Sub => {
-            let left = value + 1 + rand::random::<i32>().abs() % value;
-            let right = left - value;
+            let left = node.value + 1 + rand::random::<i32>().abs() % node.value;
+            let right = left - node.value;
 
-            evaluate_node(&mut node.children[0], left)?;
-            evaluate_node(&mut node.children[1], right)?;
+            create_child(left);
+            create_child(right);
         }
         MathOp::Mul => {
-            let divisors = (2..value).filter(|i| value % i == 0).collect::<Vec<i32>>();
+            let divisors = (2..node.value)
+                .filter(|i| node.value % i == 0)
+                .collect::<Vec<i32>>();
 
             if let Some(&left) = divisors.choose(&mut rand::thread_rng()) {
-                let right = value / left;
+                let right = node.value / left;
 
-                evaluate_node(&mut node.children[0], left)?;
-                evaluate_node(&mut node.children[1], right)?;
+                create_child(left);
+                create_child(right);
             } else {
-                node.op = MathOp::Const(Some(value));
+                node.op = MathOp::Const;
             }
         }
         MathOp::Div => {
-            let bottom = ((value as f64).sqrt().ceil() as i32) + 1;
-            let top = value * bottom;
+            let bottom = ((node.value as f64).sqrt().ceil() as i32) + 1;
+            let top = node.value * bottom;
 
-            evaluate_node(&mut node.children[0], top)?;
-            evaluate_node(&mut node.children[1], bottom)?;
+            create_child(top);
+            create_child(bottom);
         }
-        MathOp::Const(None) => {
-            node.op = MathOp::Const(Some(value));
-        }
-        MathOp::Const(Some(_)) => {
-            return Err("Constant nodes with vaules should not be evaluated".to_string());
-        }
+        MathOp::Null | MathOp::Const => {}
     }
-
-    Ok(())
 }
 
 fn format_node(node: &MathNode) -> Result<String, String> {
@@ -167,18 +164,18 @@ fn format_node(node: &MathNode) -> Result<String, String> {
     };
 
     match node.op {
-        MathOp::Const(Some(value)) => Ok(value.to_string()),
+        MathOp::Const => Ok(node.value.to_string()),
         MathOp::Add | MathOp::Mul => {
             let left = format_child(0, this_prec)?;
             let right = format_child(1, this_prec)?;
 
-            Ok(format!("{} {} {}", left, node.op, right))
+            Ok(format!("{} {} {}", left, node.op.symbol()?, right))
         }
         MathOp::Sub => {
             let left = format_child(0, this_prec)?;
             let right = format_child(1, 0)?;
 
-            Ok(format!("{} {} {}", left, node.op, right))
+            Ok(format!("{} {} {}", left, node.op.symbol()?, right))
         }
         MathOp::Div => {
             let left = format_child(0, 10000)?;
@@ -186,7 +183,7 @@ fn format_node(node: &MathNode) -> Result<String, String> {
 
             Ok(format!("\\frac{{{}}}{{{}}}", left, right))
         }
-        MathOp::Const(None) => Err("Cannot format empy constant".to_string()),
+        _ => Ok(format!("{:?}", node.op)),
     }
 }
 
@@ -206,16 +203,33 @@ pub fn get_equation(answer: i32, depth: i32, toggles: Vec<String>) -> String {
         }
     }
 
-    let mut root = MathNode::new(1);
+    let mut root = MathNode::new(MathOp::Null, answer, 1);
 
     expand_node(&mut root, depth, &ops, flags);
-
-    if let Err(msg) = evaluate_node(&mut root, answer) {
-        return msg;
-    }
 
     match format_node(&root) {
         Ok(tex) => tex,
         Err(msg) => msg,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_equation() {
+        let answer = 42;
+        let depth = 3;
+        let toggles = vec!["allowAdd", "allowSub", "allowMul", "allowDiv"];
+
+        println!(
+            "{}",
+            get_equation(
+                answer,
+                depth,
+                toggles.into_iter().map(|s| s.to_string()).collect()
+            )
+        )
     }
 }
